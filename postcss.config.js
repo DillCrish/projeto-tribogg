2748e154144cc9fa6b9955814e7ed33818af981a
Heloisa <heloisa.botelho.hb@gmail.com>
2026-02-07 01:05:32 -0300

    Refactor hero and navbar components for improved styling and structure
    - Updated hero section background to use a single image with CSS properties for better performance.
    - Adjusted typography styles in the hero section for enhanced readability and visual appeal.
    - Added a new text element in the hero section to highlight "La Tribonera".
    - Modified navbar styles to improve visibility and added a logo image for branding.
    - Cleaned up unnecessary HTML elements and improved overall layout consistency.

--- postcss.config.js
+++ postcss.config.js
@@ -2,4 +2,12 @@ module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
   },
+
+   theme: {
+      extend: {
+        backgroundImage: {
+         'hero-pattern': "url('./public/images/background-gaules.png')",
+        }
+      }
+    }
 }
