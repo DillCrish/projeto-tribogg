c0f507eb3956fba896764f34fdb369ceaa09197a
Rian Almeida <rianmatheusdasilvaalmeida@hotmail.com>
2026-02-07 00:02:58 -0300

    Integrate FontAwesome and refine Navbar styling
    - Add FontAwesome dependencies to `package.json`
    - Import `FontAwesomeModule` and icons in `navbar.ts`
    - Replace `<i>` tags with `<fa-icon>` components in `navbar.html`
    - Update Navbar layout and apply improved Tailwind CSS styling

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faTwitch} from "@fortawesome/free-brands-svg-icons/faTwitch";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+    protected readonly faTwitch = faTwitch;
+    protected readonly faUser = faUser;
 }
